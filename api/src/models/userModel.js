c7a75805b6cf419d07fb6eecbb3a26101495615e
Jamie Huang <tyhuang06@gmail.com>
2022-03-19 18:07:07 -0400

    feat(auth): register user

--- api/src/models/userModel.js
+++ api/src/models/userModel.js
@@ -3,11 +3,10 @@ import mongoose from 'mongoose';
 const userModel = mongoose.Schema(
 	{
 		name: { type: String, required: true },
-		email: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		pic: {
 			type: String,
-			required: true,
 			default:
 				'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg',
 		},
--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import { chats } from './data.js';
 import 'dotenv/config';
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 const PORT = process.env.PORT || 8000;
 
@@ -17,18 +18,12 @@ app.use(
 	})
 );
 
-// routes
+app.use(express.json()); // to accept json data
+
+app.use('/user', userRoutes);
+
 app.get('/', (req, res) => {
 	res.send('running');
-});
-
-app.get('/chat', (req, res) => {
-	res.send(chats);
-});
-
-app.get('/chat/:id', (req, res) => {
-	const singleChat = chats.find((c) => c._id === req.params.id);
-	res.send(singleChat);
 });
 
 app.listen(PORT, console.log(`Listening on PORT ${PORT}...`));
