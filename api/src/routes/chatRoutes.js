db1dab1d09414337e3d498a32d438095db0a4787
Jamie Huang <tyhuang06@gmail.com>
2022-03-19 21:36:13 -0400

    feat(api): created api for fetching all chats

--- api/src/routes/chatRoutes.js
+++ api/src/routes/chatRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { accessChat } from '../controllers/chatController.js';
+import { accessChat, fetchChats } from '../controllers/chatController.js';
 
 const router = express.Router();
 
+// Create or get chat with another user
 router.route('/').post(protect, accessChat);
-/* router.route('/').get(protect, fetchChats);
+// Get all chats for current user
+router.route('/').get(protect, fetchChats);
 
-router.route('/group').post(protect, createGroupChat);
+/*router.route('/group').post(protect, createGroupChat);
 router.route('/group/rename').put(protect, renameGroup);
 router.route('/group/remove').put(protect, removeFromGroup);
 router.route('/group/add').put(protect, addToGroup); */
