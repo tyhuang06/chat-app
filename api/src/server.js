840f0e61910f3f7a06902605da268d0f45444c7d
Jamie Huang <tyhuang06@gmail.com>
2022-03-21 11:49:55 -0400

    fix(deploy): pulled api dependencies to root

--- api/src/server.js
+++ api/src/server.js
@@ -35,12 +35,10 @@ app.use('/message', messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname1, '../client/build')));
+	app.use(express.static(path.join(__dirname1, 'client/build')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(
-			path.resolve(__dirname1, '..', 'client', 'build', 'index.html')
-		);
+		res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'));
 	});
 } else {
 	app.get('/', (req, res) => {
