d77bef887905a902f682fee08123f5ac70890316
Jamie Huang <tyhuang06@gmail.com>
2022-03-21 12:02:08 -0400

    fix(deploy): axios url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosDefault = axios.create({
-	baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000',
+	baseURL:
+		process.env.REACT_APP_API_URL ||
+		'https://mern-chatapp-deploy.herokuapp.com/',
 	withCredentials: true,
 });
 
